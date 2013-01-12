686e0fd8a2d590a9fbba75143234c067f8803c68
Michael Thiessen <electromikenetic@gmail.com>
2013-01-12 15:33:33 -0600

    Added embedded Google Maps to the popups, and added more info to them. Also update the scraper, so that it now also scrapes admission prices and addresses. Still need to add tests for the added functionality though.

--- RESTful Server/server.rb
+++ RESTful Server/server.rb
@@ -7,6 +7,7 @@ require 'date'
 LIMIT = 10
 OFFSET = 0
 
+# Parses out the parameters into an easily accessible hash
 def hashParameters params
 
 	# Create a temp array
@@ -21,6 +22,31 @@ def hashParameters params
 
 	# Convert to a hash and return
 	parameters = Hash[*parameters]
+end
+
+# Displays concert information for a single day
+def displaySingleDay displayMode
+
+	# Set up our database
+	@database = Sequel.connect('postgres://Michael@127.0.0.1:5432/concerts')
+
+	# Set variables differently depending on if we're looking at today's or tomorrow's concerts
+	if displayMode == 'Today'
+
+		# Get the concerts and the string containing the date
+		@concertList = @database.from(:concerts).where(:date => (Date.today)..(Date.today + 1)).order(:date)
+		@currentDay = Date.today.strftime("%b %-d")
+
+	else
+		@concertList = @database.from(:concerts).where(:date => (Date.today + 1)..(Date.today + 2)).order(:date)
+		@currentDay = (Date.today + 1).strftime("%b %-d")
+	end
+
+	# Set our current display mode
+	@displayMode = displayMode
+
+	# Render our page
+	erb :index
 end
 
 # Configure our server and do any initialization
@@ -38,13 +64,44 @@ end
 
 get '/' do
 
-	# Get our concert data
-	database = Sequel.connect('postgres://Michael@127.0.0.1:5432/concerts')
-	@todayList = database.from(:concerts).where(:date => (Date.today)..(Date.today + 1)).order(:date)
-	# @tomorrowList = database.from(:concerts).where(:date => (Date.today + 1)..(Date.today + 2)).order(:date)
+	displaySingleDay 'Today'
 
-	# Render our page
-	erb :index
+end
+
+get '/tomorrow' do
+
+	displaySingleDay 'Tomorrow'
+
+end
+
+get '/nextWeek' do
+
+	# Set up our database
+	@database = Sequel.connect('postgres://Michael@127.0.0.1:5432/concerts')
+
+	# We need to create an array of days, each day holding an array of concerts
+	@days = []
+
+	# We want to get 2..10 days from now
+	for i in 2...10 do
+
+		# Get the concerts for that day
+		beginDate = (Date.today + i)
+
+		# concerts = @database.from(:concerts).where(:date => beginDate..(beginDate + 1)).order(:date)
+		concerts = @database.from(:concerts).where(:date => /#{beginDate.to_s}.*/)
+
+		# Add to the list if there are some concerts
+		if concerts.count > 0
+			@days << concerts
+		else
+			puts "Not enough concerts on #{beginDate.to_s}"
+		end
+
+	end
+
+	erb :nextWeek
+
 end
 
 # Return a list of the concerts in JSON
@@ -63,7 +120,7 @@ get '/concerts' do
 
 	# Get our concert data
 	database = Sequel.connect('postgres://Michael@127.0.0.1:5432/concerts')
-	@concertList = database.from(:concerts).first(20)
+	@concertList = database.from(:concerts).all
 
 	# Now we can get the list of concerts and stuff
 	JSON.pretty_generate(@concertList)
